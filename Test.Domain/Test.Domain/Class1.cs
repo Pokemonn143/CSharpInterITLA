caab1b53995724eebf451f1ef45f353f17149742
Pokemonn143 <109120139+Pokemonn143@users.noreply.github.com>
2024-04-16 21:39:19 -0400

    test

---
+++ Test.Domain/Test.Domain/Class1.cs
@@ -0,0 +1,24 @@
+﻿namespace Test.Domain
+{
+    public abstract class Person
+    {
+
+        public string? name { get; set; }
+        public string? Lastname { get; set; }
+
+        public string? Phone { get; set; }
+
+        public string? Email { get; set; }
+
+        public virtual void Load()
+
+        {
+            Console.WriteLine("Cargando datos...");
+        }
+
+    }
+
+    
+}
+
+public abstract class employee
