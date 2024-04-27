f04d498fd5ee38832d62293b8c820207a85504bd
Pokemonn143 <109120139+Pokemonn143@users.noreply.github.com>
2024-04-27 19:56:53 -0400

    practica patron SRP

---
+++ Practica2SRP/Practica2SRP/BLL/EnviarCorreo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica2SRP.BLL
+{
+    public static class EnviarCorreo
+    {
+        public static void Enviarcorreo(string Mensaje)
+        {
+            const string Correo = "CorreoEjemplo@gmail.com";
+            const string clave = "12345";
+            var Datos = Mensaje;
+
+            Send(Datos, clave, Correo)
+            {
+
+            }
+
+
+        }
+
+
+    }
+}
---
+++ Practica2SRP/Practica2SRP/Clases/Persona.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Persona
+{
+    public Persona()
+    {
+    }
+
+    private string nombre;
+    private int edad;
+    private string direccion;
+    private string correoElectronico;
+
+    public void ImprimirDatos()
+    {
+
+        Console.WriteLine($"Nombre: {nombre}");
+        Console.WriteLine($"Edad: {edad}");
+        Console.WriteLine($"Dirección: {direccion}");
+        Console.WriteLine($"Correo electrónico: {correoElectronico}");
+    }
+
+}
