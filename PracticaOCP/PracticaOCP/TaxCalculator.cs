e08c0267f278f1707775222928316c9ff364b384
Pokemonn143 <109120139+Pokemonn143@users.noreply.github.com>
2024-05-07 23:18:32 -0400

    so far practica OCP

---
+++ PracticaOCP/PracticaOCP/TaxCalculator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaOCP
+{
+    public abstract class TaxCalculator
+    {
+       
+        public abstract decimal CalculateTax();
+
+    }
+
+    public class TaxIndia : TaxCalculator
+    {
+        public decimal TaxAmount = 10;
+        public decimal TaxeableIncome = 0;
+        public decimal income = 0;
+        public decimal deduction = 30;
+        public override decimal CalculateTax()
+        {
+            TaxeableIncome = income - deduction;
+            TaxAmount = TaxeableIncome * 1.07;
+            return TaxAmount;
+        }
+    }
+
+    public class TaxUSA: TaxCalculator
+    {
+
+        public override decimal CalculateTax()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class TaxUK : TaxCalculator
+    {
+
+        public override decimal CalculateTax()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
+
