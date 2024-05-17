1f142b0a3bc9e7b7070f81cb158e0c1565c70386
Pokemonn143 <109120139+Pokemonn143@users.noreply.github.com>
2024-05-16 20:55:20 -0400

    practica ISP

---
+++ PracticaISP/PracticaISP/IAve.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaISP
+{
+    public interface IAve
+    {
+        void Volar();
+        void Cantar();
+        void PonerHuevos();
+        
+    }
+}
